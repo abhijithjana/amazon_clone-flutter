5269ef28b28400d08033cc7e16302b657ebc8bd6
abhijithjana <obhijithjana@gmail.com>
2022-12-15 20:53:58 +0530

    home page app bar

--- lib/features/screens/home/home.dart
+++ lib/features/screens/home/home.dart
@@ -1,9 +1,12 @@
+import 'dart:ui';
+
 import 'package:amazon_clone/provider/user_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:provider/provider.dart';
+import 'package:amazon_clone/constatn/globalvariable.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -18,6 +21,72 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration:
+                const BoxDecoration(gradient: GlobalVariable.appBarGradient),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Container(
+                    padding: const EdgeInsets.only(left: 15),
+                    height: 40,
+                    child: Material(
+                      elevation: 1,
+                      borderRadius: BorderRadius.circular(7),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            prefixIcon: InkWell(
+                              onTap: () {},
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 6),
+                                child: const Icon(
+                                  Icons.search,
+                                  color: Colors.black,
+                                  size: 23,
+                                ),
+                              ),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            contentPadding: const EdgeInsets.only(top: 8),
+                            border: const OutlineInputBorder(
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(7)),
+                                borderSide: BorderSide.none),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(7)),
+                                borderSide: BorderSide(
+                                  color: Colors.black38,
+                                  width: 1,
+                                )),
+                            hintText: "Search on amazon.in",
+                            hintStyle: const TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 17,
+                            )),
+                      ),
+                    )),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                color: Colors.transparent,
+                height: 42,
+                child: Icon(
+                  Icons.mic,
+                  color: Colors.black,
+                  size: 25,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
       body: Center(
         child: Text(user.toJson()),
       ),
